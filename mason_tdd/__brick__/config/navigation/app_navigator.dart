9c4364e38041b8b53ca2964e6bda9899919c9f2b
Munawer <117267127+Munawer2022@users.noreply.github.com>
2023-07-21 22:00:03 +0500

    first commit

--- mason_tdd/__brick__/config/navigation/app_navigator.dart
+++ mason_tdd/__brick__/config/navigation/app_navigator.dart
@@ -5,5 +5,40 @@ class AppNavigator {
   //       builder: (context) => page,
   //     ),
   //   );
-  // }
+  // //
+  // push(BuildContext context, Widget page) {
+  // Navigator.of(context).push(
+  //           SlideFromRightPageRoute(
+  //             widget: page
+  //           ),
+  //         );
 }
+// }
+// class SlideFromRightPageRoute<T> extends PageRouteBuilder<T> {
+//   final Widget widget;
+
+//   SlideFromRightPageRoute({required this.widget})
+//       : super(
+//           pageBuilder: (BuildContext context, Animation<double> animation,
+//               Animation<double> secondaryAnimation) {
+//             return widget;
+//           },
+//           transitionsBuilder: (BuildContext context,
+//               Animation<double> animation,
+//               Animation<double> secondaryAnimation,
+//               Widget child) {
+//             const begin = Offset(1.0, 0.0);
+//             const end = Offset.zero;
+//             const curve = Curves.ease;
+
+//             var tween =
+//                 Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+//             var offsetAnimation = animation.drive(tween);
+
+//             return SlideTransition(
+//               position: offsetAnimation,
+//               child: child,
+//             );
+//           },
+//         );
+// }
