b33a2151aa951d1d9d0ca7ea92e9aeca1c540f29
Munawer <117267127+Munawer2022@users.noreply.github.com>
2023-10-27 16:22:24 -0700

    - add some file in mvvm

---
+++ mason_mvvm/__brick__/utils/global_instances.dart
@@ -0,0 +1,26 @@
+// import 'dart:io';
+
+// import 'package:chap_chap/resource/images.dart';
+// import 'package:chap_chap/resource/ios_android_components/platform_text_form_field.dart';
+
+// import '../resource/app_navigator.dart';
+// import '../resource/color.dart';
+// import '../resource/ios_android_components/custom_platform_back_button.dart';
+// import '../resource/ios_android_components/platform_app_check.dart';
+// import '../resource/ios_android_components/platform_button.dart';
+// import '../resource/ios_android_components/platform_scaffold_check.dart';
+// import '../resource/padding.dart';
+// import '../resource/theme.dart';
+
+// final ios = Platform.isIOS;
+// final PlatformAppCheck platformAppCheck = PlatformAppCheck();
+// final PlatformScaffoldCheck platformScaffoldCheck = PlatformScaffoldCheck();
+// final PlatformButton platformButton = PlatformButton();
+// final Style style = Style();
+// final AppNavigator appNavigator = AppNavigator();
+// final PlatformTextFormField platformTextFormField = PlatformTextFormField();
+// final CustomPlatformBackButton customPlatformBackButton =
+//     CustomPlatformBackButton();
+// final AppColors appColors = AppColors();
+// final AppPadding appPadding = AppPadding();
+// final Images images = Images();
