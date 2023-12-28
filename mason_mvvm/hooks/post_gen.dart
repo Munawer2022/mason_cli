85df1d39ff9cb4b69c10d0872cdd510f345e78ea
Munawer <117267127+Munawer2022@users.noreply.github.com>
2023-12-29 02:53:00 +0500

    - add some changes

--- mason_mvvm/hooks/post_gen.dart
+++ mason_mvvm/hooks/post_gen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
+import 'package:path/path.dart' as path;
 
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
@@ -15,6 +16,15 @@ Future<void> run(HookContext context) async {
   await Process.runSync('flutter', ['pub', 'add', 'shared_preferences'],
       runInShell: true);
   await Process.runSync('flutter', ['pub', 'get'], runInShell: true);
+  //
+  // final generate = await MasonGenerator.fromBundle(MasonBundle(
+  //     name: 'mason_mvvm',
+  //     description: 'A new brick created with the Mason CLI',
+  //     version: '0.1.0+1'));
+  // await generate.generate(DirectoryGeneratorTarget(Directory(path.join(
+  //     Directory.current.path,
+  //     'lib',
+  //     (context.vars['name'] as String).snakeCase))));
 
   progress.complete();
 }
