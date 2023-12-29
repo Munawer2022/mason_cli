097fe92b17795cb8c5cb4fc76b116e9c2d9068e2
Munawer <117267127+Munawer2022@users.noreply.github.com>
2023-12-29 19:36:39 +0500

    - add some changes

--- mason_mvvm/hooks/post_gen.dart
+++ mason_mvvm/hooks/post_gen.dart
@@ -1,32 +1,20 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
-import 'package:path/path.dart' as path;
 
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
 
   // Run `flutter packages get` after generation.
-  await Process.runSync('flutter', ['pub', 'add', 'provider'],
-      runInShell: true);
-  await Process.runSync('flutter', ['pub', 'add', 'device_preview'],
-      runInShell: true);
-  await Process.runSync('flutter', ['pub', 'add', 'flutter_screenutil'],
-      runInShell: true);
-  await Process.runSync('flutter', ['pub', 'add', 'http'], runInShell: true);
-  await Process.runSync('flutter', ['pub', 'add', 'shared_preferences'],
-      runInShell: true);
-  await Process.runSync('flutter', ['pub', 'get'], runInShell: true);
-  //
-  // final generate = await MasonGenerator.fromBundle(MasonBundle(
-  //     name: 'mason_mvvm',
-  //     description: 'A new brick created with the Mason CLI',
-  //     version: '0.1.0+1'));
-  // await generate.generate(DirectoryGeneratorTarget(Directory(path.join(
-  //     Directory.current.path,
-  //     'lib',
-  //     (context.vars['name'] as String).snakeCase))));
+  // await Process.runSync('flutter', ['pub', 'add', 'provider'],
+  //     runInShell: true);
+  // await Process.runSync('flutter', ['pub', 'add', 'device_preview'],
+  //     runInShell: true);
+  // await Process.runSync('flutter', ['pub', 'add', 'flutter_screenutil'],
+  //     runInShell: true);
+  // await Process.runSync('flutter', ['pub', 'add', 'http'], runInShell: true);
+  // await Process.runSync('flutter', ['pub', 'add', 'shared_preferences'],
+  //     runInShell: true);
+  // await Process.runSync('flutter', ['pub', 'get'], runInShell: true);
 
-  // File file = File('lib/main.dart');
-  // file.writeAsBytesSync(bytes)
   progress.complete();
 }
--- mason_mvvm_folder/hooks/post_gen.dart
+++ mason_mvvm_folder/hooks/post_gen.dart
@@ -4,26 +4,76 @@ import 'package:path/path.dart' as path;
 
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
+  final name = (context.vars['name'] as String? ?? "").trim().pascalCase;
 
-  // final generate = await MasonGenerator.fromBundle(MasonBundle(
-  //     name: 'mason_mvvm',
-  //     description: 'A new brick created with the Mason CLI',
-  //     version: '0.1.0+1'));
-  // await generate.generate(DirectoryGeneratorTarget(Directory(path.join(
-  //     Directory.current.path,
-  //     'lib',
-  //     (context.vars['name'] as String).snakeCase))));
+  // void appendAtEndOfProvidersList(String content) {
+  //   File file = File('lib/main.dart');
+  //   String fileContent = file.readAsStringSync();
 
-  void appendToFile(String content) {
-    File file = File('main.dart');
-    RandomAccessFile appendFile = file.openSync(mode: FileMode.append);
-    appendFile.writeStringSync(content);
-    appendFile.closeSync();
+  //   // Find the position of runApp(MultiProvider(providers: [
+  //   int runAppIndex = fileContent.indexOf('runApp(MultiProvider(providers: [');
+
+  //   if (runAppIndex != -1) {
+  //     int endProvidersIndex = fileContent.indexOf(']', runAppIndex);
+
+  //     if (endProvidersIndex != -1) {
+  //       String start = fileContent.substring(0, endProvidersIndex);
+  //       String end = fileContent.substring(endProvidersIndex);
+
+  //       String updatedContent = '$start\n$content\n$end';
+
+  //       file.writeAsStringSync(updatedContent);
+  //       print('Content appended at the end of providers list.');
+  //     } else {
+  //       print('End of providers list (]) not found after runApp');
+  //     }
+  //   } else {
+  //     print('runApp(MultiProvider(providers: [ not found in the file.');
+  //   }
+  // }
+  void addImportAndProvider(String importStatement, String providerStatement) {
+    File file = File('lib/main.dart');
+    String fileContent = file.readAsStringSync();
+
+    int importBlockIndex = fileContent.indexOf('import ');
+
+    if (importBlockIndex != -1) {
+      // Find the end of the import block
+      int endOfImportBlockIndex = fileContent.indexOf(';', importBlockIndex);
+      if (endOfImportBlockIndex != -1) {
+        // Insert new import at the end of existing imports
+        String updatedContent =
+            '${fileContent.substring(0, endOfImportBlockIndex + 1)}\n$importStatement\n${fileContent.substring(endOfImportBlockIndex + 1)}';
+
+        // Find the start of runApp(MultiProvider(providers: [
+        int runAppIndex =
+            fileContent.indexOf('runApp(MultiProvider(providers: [');
+        if (runAppIndex != -1) {
+          int endProvidersIndex = fileContent.indexOf(']', runAppIndex);
+
+          if (endProvidersIndex != -1) {
+            // Append the provider statement after the existing providers
+            updatedContent =
+                '${updatedContent.substring(0, endProvidersIndex)}$providerStatement\n${updatedContent.substring(endProvidersIndex)}';
+
+            file.writeAsStringSync(updatedContent);
+            print('Import and provider added successfully.');
+            return;
+          }
+        }
+      }
+    }
+
+    print('Failed to add import or provider.');
   }
 
-  String additionalContent = '\nAdditional content to add';
-  appendToFile(additionalContent);
-  progress.complete();
+  String importStatement = "import 'view_model/hello_view_model.dart';";
+
+  String providerStatement =
+      '\nChangeNotifierProvider(create: (_) => ${name}ViewModel()),';
+
+  // appendAtEndOfProvidersList(additionalContent);
+  addImportAndProvider(importStatement, providerStatement);
 
 //
 
@@ -46,7 +96,7 @@ Future<void> run(HookContext context) async {
     }
   }
 
-  String sourcePath = 'second_view.dart';
-  String destinationDirectory = 'data';
+  String sourcePath = '${name.snakeCase}_view.dart';
+  String destinationDirectory = 'lib/data';
   moveFileToDirectory(sourcePath, destinationDirectory);
 }
