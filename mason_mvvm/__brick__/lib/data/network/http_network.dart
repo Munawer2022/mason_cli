90c4059cb1e8f521577568c197549a4338e92b00
Munawer <117267127+Munawer2022@users.noreply.github.com>
2024-06-02 17:29:02 +0500

    - add mvvm https changes

--- mason_mvvm/__brick__/lib/data/network/http_network.dart
+++ mason_mvvm/__brick__/lib/data/network/http_network.dart
@@ -3,31 +3,38 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import '/data/app_exceptions.dart';
 {{#auth}}
 import '/view_model/local/local_user_info_store_view_model.dart';
 {{/auth}}
-import '../app_exceptions.dart';
 import 'network_base_api_services.dart';
 
 class HttpNetwork implements NetworkBaseApiServices {
   {{#auth}}
   final LocalUserInfoStoreViewModel _userInfoDataSources;
+  HttpNetwork(this._userInfoDataSources);
+
+  Future<String> _getToken() async {
+    while (_userInfoDataSources.userInfo.token.isEmpty) {
+      await Future.delayed(Duration.zero);
+    }
+    return _userInfoDataSources.userInfo.token;
+  }
   {{/auth}}
-  HttpNetwork(
-    {{#auth}}
-    this._userInfoDataSources
-    {{/auth}}
-    );
+
   @override
   Future<T> getApi<T>({required String url}) async {
+    {{#auth}}
+    String token = await _getToken();
+    print("Using token: $token");
+    {{/auth}}
+
     T responseJson;
     try {
       final response = await http.get(
         Uri.parse(url),
         {{#auth}}
-        headers: {
-          'Authorization': 'Bearer ${_userInfoDataSources.userInfo.token}'
-        },
+        headers: {'Authorization': 'Bearer $token'},
         {{/auth}}
       ).timeout(const Duration(seconds: 10));
       responseJson = returnResponse<T>(response);
@@ -68,6 +75,10 @@ class HttpNetwork implements NetworkBaseApiServices {
       {required String url,
       required Map<String, dynamic> body,
       Map<String, String>? headers}) async {
+        {{#auth}}
+    String token = await _getToken();
+    print("Using token: $token");
+        {{/auth}}
     T responseJson;
     try {
       Response response = await patch(
@@ -76,7 +87,8 @@ class HttpNetwork implements NetworkBaseApiServices {
         headers: headers 
         {{#auth}}
         ??
-            {'Authorization': 'Bearer ${_userInfoDataSources.userInfo.token}'},
+            // {'Authorization': 'Bearer ${_userInfoDataSources.userInfo.token}'},
+            {'Authorization': 'Bearer $token'},
         {{/auth}}
       ).timeout(const Duration(seconds: 10));
 
@@ -110,3 +122,25 @@ class HttpNetwork implements NetworkBaseApiServices {
     }
   }
 }
+//   T returnResponse<T>(http.Response response) {
+//     switch (response.statusCode) {
+//       case 200:
+//         T responseJson = jsonDecode(response.body);
+//         return responseJson;
+//       case 400:
+//         final Map<String, dynamic> responseJson = jsonDecode(response.body);
+//         throw BadRequestException(responseJson['message']);
+//       case 401:
+//       case 403:
+//         final Map<String, dynamic> responseJson = jsonDecode(response.body);
+//         throw UnauthorizedException(responseJson['message']);
+//       case 500:
+//       case 404:
+//         throw FetchDataException(
+//             'Error occurred while communicating with server. Status code: ${response.statusCode}');
+//       default:
+//         throw FetchDataException(
+//             'Error occurred while communicating with server. Status code: ${response.statusCode}');
+//     }
+//   }
+// }
