f9e537c3db92437f9c5808249b7913165a90caf2
Munawer <117267127+Munawer2022@users.noreply.github.com>
2024-11-16 12:15:37 +0500

    remove some data source and use case unuse files remove and remove nothing some unuse files

--- mason_tdd/__brick__/lib/domain/repositories/{{#isNoThing}}{{folder_name}}{{/isNoThing}}/{{folder_name}}_base_api_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import '/domain/entities/{{folder_name}}/mock_{{folder_name}}_model.dart';
-import '/domain/failures/{{folder_name}}/{{folder_name}}_failure.dart';
-
-abstract class {{class_name}}BaseApiService {
-  Future<Either<{{class_name}}Failure, Mock{{class_name}}Model>> {{folder_name_camelCase}}();
-}
