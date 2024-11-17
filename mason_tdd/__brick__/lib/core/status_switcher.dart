7bd19cd87f171d5640d8b84fe5a2200e8c710f1d
Munawer <117267127+Munawer2022@users.noreply.github.com>
2024-11-17 19:07:46 +0500

    add some changes

--- mason_tdd/__brick__/lib/core/status_switcher.dart
+++ mason_tdd/__brick__/lib/core/status_switcher.dart
@@ -1,20 +1,19 @@
 import '/core/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '/config/response/api_response.dart';
 import '/config/response/status.dart';
 
 class StatusSwitcher<T> extends StatelessWidget {
   final ApiResponse<T> response;
-  final Widget Function(BuildContext context) onLoading;
+  final Widget Function(BuildContext context)? onLoading;
   final Widget Function(BuildContext context, String message)? onError;
   final Widget Function(BuildContext context, T data) onCompleted;
 
   const StatusSwitcher(
       {super.key,
       required this.response,
-      required this.onLoading,
+      this.onLoading,
       this.onError,
       required this.onCompleted});
 
@@ -22,25 +21,26 @@ class StatusSwitcher<T> extends StatelessWidget {
   Widget build(BuildContext context) {
     switch (response.status) {
       case Status.LOADING:
-        return onLoading(context);
+        return onLoading != null
+            ? onLoading!(context)
+            : const Center(child: CircularProgressIndicator.adaptive());
       case Status.ERROR:
         return onError != null
             ? onError!(context, response.message)
             : Center(
                 child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40.w),
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
                     child: Text(response.message,
                         textAlign: TextAlign.center,
                         style: context.textTheme.bodyLarge)));
-
-      // case Status.COMPLETED:
-      //   return onCompleted(context, response.data);
       case Status.COMPLETED:
         if (response.data == null ||
             (response.data is Iterable &&
                 (response.data as Iterable).isEmpty)) {
-          return Text('No data',
-              style: context.textTheme.bodyLarge, textAlign: TextAlign.center);
+          return Center(
+              child: Text('No data',
+                  style: context.textTheme.bodyLarge,
+                  textAlign: TextAlign.center));
         }
         return onCompleted(context, response.data);
       default:
