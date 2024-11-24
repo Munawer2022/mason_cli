cbd3cfb08e61e6366860406d52d307232ed90d1c
Munawer <117267127+Munawer2022@users.noreply.github.com>
2024-11-24 13:03:10 +0500

    remove post success model

--- mason_tdd/__brick__/lib/domain/repositories/{{#isPost}}{{folder_name}}{{/isPost}}/{{folder_name}}_base_api_service.dart
+++ mason_tdd/__brick__/lib/domain/repositories/{{#isPost}}{{folder_name}}{{/isPost}}/{{folder_name}}_base_api_service.dart
@@ -1,9 +1,8 @@
 import 'package:fpdart/fpdart.dart';
-import '/data/models/{{folder_name}}/{{folder_name}}_success_model.dart';
 import '/domain/failures/{{folder_name}}/{{folder_name}}_failure.dart';
 
 
 abstract class {{class_name}}BaseApiService {
-  Future<Either<{{class_name}}Failure, {{class_name}}SuccessModel>> {{folder_name_camelCase}}(
+  Future<Either<{{class_name}}Failure, Map<String,dynamic>>> {{folder_name_camelCase}}(
     {required Map<String, dynamic> body});
 }
