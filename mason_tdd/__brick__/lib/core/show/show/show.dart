759628b087d91a4a5617c4d61be85f32e4537519
Munawer <117267127+Munawer2022@users.noreply.github.com>
2025-10-27 21:30:24 +0500

    Remove ShowMaterialBanner mixins to streamline the Show class and improve code maintainability.

--- mason_tdd/__brick__/lib/core/show/show/show.dart
+++ mason_tdd/__brick__/lib/core/show/show/show.dart
@@ -1,9 +1,3 @@
-import 'show_material_banner.dart';
 import 'show_snack_bar.dart';
 
-class Show
-    with
-        ShowSnackBarError,
-        ShowSnackBarSuccess,
-        ShowMaterialBannerSuccess,
-        ShowMaterialBannerError {}
+class Show with ShowSnackBarError, ShowSnackBarSuccess {}
